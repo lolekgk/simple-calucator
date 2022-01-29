1c42ee6caa7d5196cb2ed1de0b2745e791e019a2
appuser <appuser@journey2-7649fc7c66-jg2vs>
2022-01-29 12:39:00 +0000

    starter project

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def is_number(str):
+    pass
+
+
+def convert_number(str):
+    pass
+
+
+def is_valid_operator(operator):
+    pass
+
+
+def ask_for_a_number(force_valid_input):
+    pass
+
+
+def ask_for_an_operator(force_valid_input):
+    pass
+
+
+def calc(operator, a, b):
+    pass
+
+
+def simple_calculator():
+    pass
+
+
+if __name__ == '__main__':
+    simple_calculator()
