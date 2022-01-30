cc85f8456ecca8d4e420e5854760e6443f143bdb
lolekgk <karol.gajda97@gmail.com>
2022-01-30 21:04:51 +0100

    Fixed ask_for_a_number() function, ask_for_and_operator() function, added calc() function.

--- calculator.py
+++ calculator.py
@@ -5,6 +5,7 @@ def is_number(str):
     except ValueError:
         return False
 
+
 def convert_number(str): 
     float_number = float(str) 
     return float_number
@@ -17,37 +18,49 @@ def is_valid_operator(operator):
     else:
         return False
 
+
 def ask_for_a_number(force_valid_input):
-    # gdy is nnumber nie jest true petla sie powtarza do momentu podania prawidłowego stringa
     while True:
         str_num = input('Please provide a number! ')
-        is_number(str_num)
         if is_number(str_num):
             return convert_number(str_num)
-        elif not force_valid_input: # w przypadku podania force_valid_input = False wykona się ta instrukcja - elif = True
+# w przypadku podania force_valid_input = False wykona się ta instrukcja - elif = True
+        elif not force_valid_input: 
             return None
         print("This didn't look like a number, try again.")
         
     
-      
 def ask_for_an_operator(force_valid_input):
-    while force_valid_input == True:
+    while True:
         input_operator = input('Please provide an operator (one of +, -, *, /)! ')
-        is_valid_operator(input_operator)
         if is_valid_operator(input_operator):
             return input_operator
         elif not force_valid_input: 
             return None
         print("Unknown operator.")
     
+
 def calc(operator, a, b):
-    pass
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    elif operator == '/':
+        if b != 0:
+            return a / b
+        else:
+            print("Error: Division by zero")
+            return None
+        
 
 
 def simple_calculator():
     while True:
         a = ask_for_a_number(0)
-        if not a: #jezeli a bedzie False - zwrocone zostanie None z funkcji a_f_n, nastapi przerwanie programu
+#jezeli a = False - zwrocone zostanie None z funkcji a_f_n, nastapi przerwanie programu - None = False
+        if not a: 
             break
         op = ask_for_an_operator(1)
         b = ask_for_a_number(1)
@@ -55,7 +68,6 @@ def simple_calculator():
         if result:
             print(f"The result is {result}")
         
-
 
 if __name__ == '__main__':
     simple_calculator()
